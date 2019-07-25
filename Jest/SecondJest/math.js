59c1d32b1e38e03608608e50138b736469301bcd
dhruv5201 <dhrubajitdas@gmail.com>
2019-07-25 11:08:35 +0530

    test

--- Jest/SecondJest/math.js
+++
@@ -1,9 +0,0 @@
-
-const mathFunctions= {
-    subs : ( a , b ) => a - b ,
-    devide : ( a, b ) => a / b ,
-    sum : ( a , b ) => a + b ,
-    mult : ( a , b ) => a * b 
-}
-
-module.exports = mathFunctions;
